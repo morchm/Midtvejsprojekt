c51f85be57f225d20f35e878f1977ae89072e305
morchm <mai.lykke.moerch@gmail.com>
2023-09-05 13:58:47 +0200

    update

--- vite-project/src/app.ts
+++ vite-project/src/app.ts
@@ -1,3 +1,5 @@
+import {images} from "../assets/json/data.json";
+
 class App {
   constructor() {
     //Container 
@@ -5,15 +7,39 @@ class App {
     containerCard.className = "div";
     document.body.appendChild(containerCard);
 
-    //Img-element
-    const img = document.createElement("img");
-    containerCard.appendChild(img);
+    //Slider-div
+    const slider = document.createElement("div");
+    slider.className = "slider"
+    containerCard.appendChild(slider)
+
+    images.forEach((value, index)=>{
+      //Img-element
+      const img = document.createElement("img");
+      img.setAttribute ("src", `${value.img}`)
+      slider.appendChild(img);
+    })
 
     //Text-element
-    const textCard = document.createElement("figcaption");
+    const textCard = document.createElement("div");
     textCard.id = "card__text";
     containerCard.appendChild(textCard);
 
+    const titleCard = document.createElement("h1");
+    titleCard.id = "card__title";
+    titleCard.innerHTML = images.title;
+    textCard.appendChild(titleCard);
+
+
+    //Buttons
+    const buttonBackCard = document.createElement("button");
+    buttonBackCard.id = "card__button--back";
+    textCard.appendChild(buttonBackCard)
+
+    const buttonForwardCard = document.createElement("button");
+    buttonForwardCard.id = "card__button--forward";
+    textCard.appendChild(buttonForwardCard)
+    
+    
     
   }
 }
